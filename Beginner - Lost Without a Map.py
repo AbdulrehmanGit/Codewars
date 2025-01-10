cf0aa3dcf242ed7eddb901ac9b35e39c572d817e
AbdulrehmanGit <61945830+AbdulrehmanGit@users.noreply.github.com>
2025-01-10 15:35:54 +0500

    Create Beginner - Lost Without a Map.py

---
+++ Beginner - Lost Without a Map.py
@@ -0,0 +1,13 @@
+# Description:
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+
+def maps(a):
+    b= []
+    for i in a:
+        b.append(i*2)
+    return b
+    
